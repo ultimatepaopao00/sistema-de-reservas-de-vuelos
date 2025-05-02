b17b23d8e11fce7aab03c11299dc315f13b0f45b
ultimatepaopao00 <paolamariamayen@gmail.com>
2025-05-02 17:17:12 -0600

    clase Vuelo
    Agregué la clase Vuelo

--- SistemaDeReservasDeVuelos/src/sistemadereservasdevuelos/SistemaDeReservasDeVuelos.java
+++ SistemaDeReservasDeVuelos/src/sistemadereservasdevuelos/SistemaDeReservasDeVuelos.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package sistemadereservasdevuelos;
 
-/**
- *
- * @author paola
- */
+
 public class SistemaDeReservasDeVuelos {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
+        
     }
     
 }
---
+++ SistemaDeReservasDeVuelos/src/sistemadereservasdevuelos/Vuelo.java
@@ -0,0 +1,26 @@
+package sistemadereservasdevuelos;
+
+public class Vuelo {
+
+    //atributos de la clase "vuelo" aplicando encapsulamiento
+    private String idVuelo;
+    private String origenVuelo;
+    private String destinoVuelo;
+    private String fechaVuelo;
+    private String horaVuelo;
+    private int duracionVuelo;
+    
+    //constructor
+    
+    
+    
+    //getters y setters
+    
+    
+    
+    
+    //metodos/ funciones
+    
+    
+
+}
