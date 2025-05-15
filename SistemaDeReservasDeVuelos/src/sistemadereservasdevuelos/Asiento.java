21603829f1fc52b011f0e89ee8deaa77ac6e0bcc
Genesisaldana7 <genesisyazira07@gmail.com>
2025-05-14 19:22:10 -0600

    codigo

--- SistemaDeReservasDeVuelos/src/sistemadereservasdevuelos/Asiento.java
+++ SistemaDeReservasDeVuelos/src/sistemadereservasdevuelos/Asiento.java
@@ -3,14 +3,20 @@ package sistemadereservasdevuelos;
 public class Asiento {
     private int numero;
     private boolean disponible;
+    private String clase;
 
     public Asiento(int numero) {
         this.numero = numero;
+        this.clase = clase;
         this.disponible = true;
     }
 
     public int getNumero() {
         return numero;
+    }
+
+    public String getClase() {
+        return clase;
     }
 
     public boolean isDisponible() {
@@ -23,6 +29,15 @@ public class Asiento {
             System.out.println("Asiento #" + numero + " reservado con exito.");
         } else {
             System.out.println("El asiento #" + numero + " se encuentra reservado.");
+        }
+    }
+
+    public void liberarAsiento() {
+        if (!disponible) {
+            disponible = true;
+            System.out.println("Reserva del asiento #" + numero + " cancelada.");
+        } else {
+            System.out.println("El asiento #" + numero + " ya está disponible.");
         }
     }
 }
