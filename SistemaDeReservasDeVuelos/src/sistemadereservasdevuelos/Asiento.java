bee2015a61e4c40ce90408b596ec7415e87d09dd
Genesisaldana7 <genesisyazira07@gmail.com>
2025-05-14 18:53:11 -0600

    codigo

--- SistemaDeReservasDeVuelos/src/sistemadereservasdevuelos/Asiento.java
+++ SistemaDeReservasDeVuelos/src/sistemadereservasdevuelos/Asiento.java
@@ -8,3 +8,21 @@ public class Asiento {
         this.numero = numero;
         this.disponible = true;
     }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void reservar() {
+        if (disponible) {
+            disponible = false;
+            System.out.println("Asiento #" + numero + " reservado con exito.");
+        } else {
+            System.out.println("El asiento #" + numero + " se encuentra reservado.");
+        }
+    }
+}
