38551a3802191cb8a1ad5d0d17e46886242cf8a9
Genesisaldana7 <genesisyazira07@gmail.com>
2025-05-22 07:59:49 -0600

    main

--- SistemaDeReservasDeVuelos/src/sistemadereservasdevuelos/SistemaDeReservasDeVuelos.java
+++ SistemaDeReservasDeVuelos/src/sistemadereservasdevuelos/SistemaDeReservasDeVuelos.java
@@ -1,10 +1,11 @@
 package sistemadereservasdevuelos;
 
-
+import java.util.Scanner;
 public class SistemaDeReservasDeVuelos {
 
   
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         
         
         
